9bd73eb72e1975fa8e84e7829fcbc0acd7f39a2b
Pablo Díaz <pdiaz9731@gmail.com>
2024-01-06 18:02:02 -0600

    Inicio de proyecto

---
+++ CarritoDeCompras/src/main/java/com/mycompany/carritodecompras/CarritoDeCompras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carritodecompras;
+
+/**
+ *
+ * @author pjdiaz
+ */
+public class CarritoDeCompras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
